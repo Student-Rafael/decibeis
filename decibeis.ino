4ccbb8b24ca877e9d15977412e97d2f7f58ae055
Student-Rafael <91rafaels@gmail.com>
2024-11-03 22:07:52 -0300

    Implementa controle dos Leds

--- decibeis.ino
+++ decibeis.ino
@@ -1,4 +1,7 @@
 const int PinoSensor = A0; // Pino Analógico de Entrada 0 (Saída do sensor KY-038)
+const int ledVerde = 3;
+const int ledAmarelo = 4;
+const int ledVermelho = 5;
  
 int ValorSensor = 0;
 int valorMaior = 0;
@@ -8,6 +11,10 @@ int cont = 0;
  
 void setup() {
   Serial.begin(9600); // Inicializa a comunicação serial
+    // Configuração dos pinos dos LEDs
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
 }
  
 void loop() {
@@ -36,5 +43,29 @@ void loop() {
   Serial.print("Intensidade: ");
   Serial.print(dB);
   Serial.println(" dB");
+// Controlando LEDs com base no valor de decibéis
+  if (dB < 48) {
+    digitalWrite(ledVerde, LOW);    // Som baixo, não acende o LED
+    digitalWrite(ledAmarelo, LOW);
+    digitalWrite(ledVermelho, LOW);
+  }  
+    else if (dB < 65) {
+    digitalWrite(ledVerde, HIGH);   // Som considerado, acende o LED Verde
+    digitalWrite(ledAmarelo, LOW);  
+    digitalWrite(ledVermelho, LOW);
+  } else if (dB < 80) {
+    digitalWrite(ledVerde, HIGH);   
+    digitalWrite(ledAmarelo, HIGH);   // Som médio, acende o LED amarelo
+    digitalWrite(ledVermelho, LOW); 
+  
+  } else {
+    digitalWrite(ledVerde, HIGH);
+    digitalWrite(ledAmarelo, HIGH);  
+    digitalWrite(ledVermelho, HIGH);  // Som alto, acende o LED Vermelho
+  }
+ 
+  valorMaior = 0; // Reinicia o valor máximo para a próxima leitura
+ 
+  delay(10);  // Delay reduzido para 10 ms para respostas mais rápidas
 
 }
